0fe7e73186cd1422a4551f47292c2abdb6b580b6
deepdishattack <36520677+deepdishattack@users.noreply.github.com>
2018-03-01 15:57:45 -0600

    Add files via upload

---
+++ Shooting.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+
+    public int weaponSelect = 0;
+
+    public float shotSpeed = 40f;
+
+    
+    public float reloadTime = 5f;
+
+    GameObject prefab0;
+    GameObject prefab1;
+    GameObject prefab2;
+    GameObject prefab3;
+    GameObject prefab4;
+
+    // Use this for initialization
+    void Start () {
+        prefab0 = Resources.Load("empty") as GameObject;
+        prefab1 = Resources.Load("red") as GameObject;
+        prefab2 = Resources.Load("blue") as GameObject;
+        prefab3 = Resources.Load("yellow") as GameObject;
+        prefab4 = Resources.Load("green") as GameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+                reloadTime += Time.deltaTime;
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            //empty
+            if ((weaponSelect == 0) && (reloadTime >= 2))
+            {
+
+                GameObject Empty = Instantiate(prefab0) as GameObject;
+                Empty.transform.position = transform.position + Camera.main.transform.forward * 2;
+                Rigidbody rb = Empty.GetComponent<Rigidbody>();
+                rb.velocity = transform.up * shotSpeed;// need to add the 90 degrees;
+                reloadTime = 0;
+            }
+
+
+            //red
+            if (weaponSelect == 1)
+            {
+                GameObject red = Instantiate(prefab1) as GameObject;
+                red.transform.position = transform.position + Camera.main.transform.forward * 2;
+                Rigidbody rb = red.GetComponent<Rigidbody>();
+                rb.velocity = transform.up * shotSpeed;
+                weaponSelect = 0;
+                reloadTime = 0;
+            }
+
+
+            //blue
+            if (weaponSelect == 2)
+            {
+                GameObject blue = Instantiate(prefab2) as GameObject;
+                blue.transform.position = transform.position + Camera.main.transform.forward * 2;
+                Rigidbody rb = blue.GetComponent<Rigidbody>();
+                rb.velocity = transform.up * shotSpeed;
+                weaponSelect = 0;
+                reloadTime = 0;
+            }
+
+
+            //yellow
+            if (weaponSelect == 3)
+            {
+                GameObject yellow = Instantiate(prefab3) as GameObject;
+                yellow.transform.position = transform.position + Camera.main.transform.forward * 2;
+                Rigidbody rb = yellow.GetComponent<Rigidbody>();
+                rb.velocity = transform.up * shotSpeed;
+                weaponSelect = 0;
+                reloadTime = 0;
+            }
+
+
+            //green
+            if (weaponSelect == 4)
+            {
+                GameObject green = Instantiate(prefab4) as GameObject;
+                green.transform.position = transform.position + Camera.main.transform.forward * 2;
+                Rigidbody rb = green.GetComponent<Rigidbody>();
+                rb.velocity = transform.up * shotSpeed;
+                weaponSelect = 0;
+                reloadTime = 0;
+            }
+        }
+
+    }
+}
